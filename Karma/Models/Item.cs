f611c613c5b5e0a71f5e56a3807b02a425143b11
bamboozlegui <kartoffelgutgut@gmail.com>
2021-09-21 19:12:13 +0300

    add users.json and Item.cs

---
+++ Karma/Models/Item.cs
@@ -0,0 +1,23 @@
+
+using System.Text.Json.Serialization;
+
+namespace Karma.Models
+{
+    public class Item
+    {
+        public string Name { get; set; }
+
+        public string City { get; set; }
+
+        [JsonPropertyName("phone_number")]
+        public string PhoneNumber { get; set; }
+
+        [JsonPropertyName("item_title")]
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Picture { get; set;}
+    }
+}
