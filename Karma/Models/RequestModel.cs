7b92ddf2828b9c50e9963351f7854c0a71729416
disoversity <sinkevicedwin@gmail.com>
2021-09-21 23:37:43 +0300

    Modify property namings

--- Karma/Models/RequestModel.cs
+++ Karma/Models/RequestModel.cs
@@ -2,16 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Text.Json.Serialization;
 
 namespace Karma.Models
 {
     public class RequestModel
     {
-        public string RequestName { get; set; }
-        public string Description { get; set; }
+         public string RequesterName { get; set; }
 
-        public string BuyerName { get; set; }
-        public string PhoneNumber { get; set; }
         public string City { get; set; }
+
+        [JsonPropertyName("phone_number")]
+        public string PhoneNumber { get; set; }
+
+        [JsonPropertyName("item_title")]
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
     }
 }
