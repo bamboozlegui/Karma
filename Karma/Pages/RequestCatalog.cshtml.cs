90e9a4d216c2044135198401d27e93282f983ece
disoversity <sinkevicedwin@gmail.com>
2021-09-22 00:02:18 +0300

    Add RequestCatalog.cshtml.cs

---
+++ Karma/Pages/RequestCatalog.cshtml.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Karma.Services;
+using Karma.Models;
+
+namespace Karma.Pages
+{
+    public class GetRequestsModel : PageModel
+    {
+        private readonly ILogger<RequestModel> _logger;
+        public JsonFileRequestService RequestService;   
+        public IEnumerable<RequestModel> Requests { get; private set; }
+
+        public GetRequestsModel(
+            ILogger<RequestModel> logger,
+            JsonFileRequestService requestService)
+        {
+            _logger = logger;
+            RequestService = requestService;
+        }
+
+        public void OnGet()
+        {
+            Requests = RequestService.GetRequests();
+        }
+    }
+}
