0795a267a1c6c0d6ffcfee4aaf1918725a8ef332
disoversity <sinkevicedwin@gmail.com>
2021-09-22 14:19:17 +0300

    Add a super abstract class PostModel.cs

--- Karma/Models/SubmitModel.cs
+++ Karma/Models/SubmitModel.cs
@@ -4,20 +4,12 @@ using System.Text.Json.Serialization;
 
 namespace Karma.Models
 {
-    public class SubmitModel
+    public class SubmitModel : PostModel, IHasJsonFile
     {
-        public string SubmitterName { get; set; }
-
-        public string City { get; set; }
-
-        public string PhoneNumber { get; set; }
-
-        public string Title { get; set; }
-
-        public string Description { get; set; }
-
         public string Picture { get; set;}
         
         public override string ToString() => JsonSerializer.Serialize<SubmitModel>(this);
+        public string GetJsonName() => "items.json";
+
     }
 }
