f371b6d010dc7e3007a933f0f6368008c4234f77
karoluzas <liskaro23@gmail.com>
2021-10-04 20:20:07 +0300

    Index page preview posts

--- Karma/Pages/Index.cshtml.cs
+++ Karma/Pages/Index.cshtml.cs
@@ -5,21 +5,43 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Karma.Models;
+using Karma.Services;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 
 namespace Karma.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        //Request dalykai
+        [BindProperty]
+        public RequestModel RequestItem { get; set; }
+        public JsonFilePostService<RequestModel> RequestService;
+        public IEnumerable<RequestModel> Requests { get; private set; }
+        
+        //Submit dalykai
+        [BindProperty]
+        public SubmitModel SubmitItem { get; set; }
+        [BindProperty]
+        public IFormFile Photo { get; set; }
+        public JsonFilePostService<SubmitModel> SubmitService;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IEnumerable<SubmitModel> Submits { get; private set; }
+
+        public IndexModel(JsonFilePostService<RequestModel> requestService, JsonFilePostService<SubmitModel> submitService)
         {
-            _logger = logger;
+            RequestService = requestService;
+            SubmitService = submitService;
         }
+
+
+        private readonly ILogger<IndexModel> _logger;
 
         public void OnGet()
         {
-
+            Requests = RequestService.GetPosts();
+            Submits = SubmitService.GetPosts();
         }
     }
 }
