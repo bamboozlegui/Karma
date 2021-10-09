2be7a4a76f87b4b92910d5437a3bdec898315178
disoversity <sinkevicedwin@gmail.com>
2021-10-09 13:01:14 +0300

    Add JsonFileItemService : JsonFilePostService

---
+++ Karma/Services/JsonFileItemService.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Karma.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace Karma.Services
+{
+    public class JsonFileItemService : JsonFilePostService<ItemPost>
+    {
+	public JsonFileItemService(JsonPictureService pictureService,
+				   IWebHostEnvironment webHostEnvironment)
+	{
+	    WebHostEnvironment = webHostEnvironment;
+            PictureService = pictureService;
+        }
+
+	public IWebHostEnvironment WebHostEnvironment { get; }
+
+	public JsonPictureService PictureService { get; }
+
+	internal override string JsonFileName
+	{
+	    get { return Path.Combine(WebHostEnvironment.ContentRootPath, "data", (new ItemPost()).GetJsonName()); }
+        }
+
+	public override void AddPost(ItemPost post, IFormFile photo)
+	{
+	    if (photo != null)
+	    {
+		if (post.Picture != null) //If our Post already has a picture path string, we should delete it first to upload a new one
+		{
+		    string filePath = Path.Combine(WebHostEnvironment.WebRootPath, "images", post.Picture);
+		    System.IO.File.Delete(filePath);
+		}
+
+		post.Picture = PictureService.ProcessUploadedFile(WebHostEnvironment, photo); //Check definition
+	    }
+	    post.Date = DateTime.Now;
+	    post.ID = Guid.NewGuid().ToString();
+
+	    IEnumerable<ItemPost> Submits = GetPosts().
+	    Append<ItemPost>(post);
+
+	    Submits = Submits.OrderByDescending(post => post.State).ThenByDescending(post => post.Title);
+
+	    RefreshPosts(Submits);
+	}
+
+	public override void DeletePost(string id)
+	{
+	    throw new System.NotImplementedException();
+        }
+
+        public override void UpdatePost(ItemPost newPost, string id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
