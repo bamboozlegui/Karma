9bcae5d9a6a728d446556c3bc304f4d1411ddc73
karoluzas <liskaro23@gmail.com>
2021-10-25 12:35:04 +0300

    Scaffold login and register

---
+++ Karma/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Karma.Areas.Identity.Data;
+using Karma.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Karma.Areas.Identity.IdentityHostingStartup))]
+namespace Karma.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<KarmaDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("KarmaDbContextConnection")));
+
+                services.AddDefaultIdentity<KarmaUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<KarmaDbContext>();
+            });
+        }
+    }
+}
