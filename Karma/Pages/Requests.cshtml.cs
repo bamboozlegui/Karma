0811de1a6fbdd3317a19632f7e028916bd2eb66f
karoluzas <liskaro23@gmail.com>
2021-10-27 14:16:14 +0300

    Testing getting data from server
    Figured out how to get data from database but now need to figure out how to make so that when requesting an item the logged in user does not need to give name

--- Karma/Pages/Requests.cshtml.cs
+++ Karma/Pages/Requests.cshtml.cs
@@ -2,21 +2,29 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Karma.Models;
 using Karma.Services;
+using Microsoft.AspNetCore.Identity;
+using Karma.Areas.Identity.Data;
+using System.Web;
 
 namespace Karma.Pages
 {
     public class RequestsModel : PageModel
     {
+
         [BindProperty]
         public RequestPost Item { get; set; }
 
         private JsonFileRequestService RequestService { get; }
 
         public IEnumerable<RequestPost> Requests { get; private set; }
+        
+        public string sqlConnectionString = "Server=(localdb)\\mssqllocaldb;Database=Karma;Trusted_Connection=True;MultipleActiveResultSets=true";
+
 
         public RequestsModel(JsonFileRequestService requestService)
         {
@@ -26,6 +34,30 @@ namespace Karma.Pages
         public void OnGet()
         {
             Requests = RequestService.GetPosts();
+
+
+
+            // Further testing required
+            using(SqlConnection conn = new SqlConnection(sqlConnectionString))
+            {
+                SqlCommand cmd = new SqlCommand("SELECT UserName, FirstName FROM Karma.dbo.AspNetUsers", conn);
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        if (HttpContext.User.Identity.Name == reader.GetString(0))
+                        {
+                            System.Diagnostics.Debug.WriteLine(reader.GetString(1));
+                            break;
+                        }
+                    }
+                }              
+            }
+
+
         }
 
         public IActionResult OnPostDelete(string id)
