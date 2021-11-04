fc72c103f32a984016c2fb13d639dc6667bcb669
karoluzas <liskaro23@gmail.com>
2021-11-04 20:27:23 +0200

    Transfer inbox, outbox, myitems, myrequests into user profile menu

--- Karma/Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
+++ Karma/Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
@@ -24,6 +24,14 @@ namespace Karma.Areas.Identity.Pages.Account.Manage
 
         public static string TwoFactorAuthentication => "TwoFactorAuthentication";
 
+        public static string Outbox => "Outbox";
+        
+        public static string Inbox => "Inbox";
+
+        public static string MyItems => "MyItems";
+        
+        public static string MyRequests => "MyRequests";
+
         public static string IndexNavClass(ViewContext viewContext) => PageNavClass(viewContext, Index);
 
         public static string EmailNavClass(ViewContext viewContext) => PageNavClass(viewContext, Email);
@@ -39,6 +47,14 @@ namespace Karma.Areas.Identity.Pages.Account.Manage
         public static string PersonalDataNavClass(ViewContext viewContext) => PageNavClass(viewContext, PersonalData);
 
         public static string TwoFactorAuthenticationNavClass(ViewContext viewContext) => PageNavClass(viewContext, TwoFactorAuthentication);
+
+        public static string OutboxNavClass(ViewContext viewContext) => PageNavClass(viewContext, Outbox);
+
+        public static string InboxNavClass(ViewContext viewContext) => PageNavClass(viewContext, Inbox);
+
+        public static string MyItemsNavClass(ViewContext viewContext) => PageNavClass(viewContext, MyItems);
+
+        public static string MyRequestsNavClass(ViewContext viewContext) => PageNavClass(viewContext, MyRequests);
 
         private static string PageNavClass(ViewContext viewContext, string page)
         {
