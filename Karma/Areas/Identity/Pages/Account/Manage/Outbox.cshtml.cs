3d8e910d5af17da81b60015430337f4cd2a320f4
disoversity <sinkevicedwin@gmail.com>
2021-11-09 07:19:12 +0200

    Apply new GET requests and promote cohesion

--- Karma/Areas/Identity/Pages/Account/Manage/Outbox.cshtml.cs
+++ Karma/Areas/Identity/Pages/Account/Manage/Outbox.cshtml.cs
@@ -1,6 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using Karma.Models;
 using Karma.Services;
@@ -12,20 +16,25 @@ namespace Karma.Areas.Identity.Pages.Account
     public class OutboxModel : PageModel
     {
 
-        public OutboxModel(IMessageRepository messageService)
+        public OutboxModel(HttpClient httpClient)
         {
-            MessageService = messageService;
+            HttpClient = httpClient;
         }
 
         public List<Message> Outbox { get; set; }
-        public IMessageRepository MessageService { get; }
+        public HttpClient HttpClient { get; }
 
         public async Task<IActionResult> OnGetAsync()
         {
-            Outbox = (await MessageService.GetMessages()).Where(delegate (Message m)
-            {
-                return m.FromEmail == HttpContext.User.Identity.Name;
-            }).ToList();
+            var email = User.Identity.Name;
+
+            Outbox = await HttpClient.GetFromJsonAsync<List<Message>>($"https://localhost:5001/api/messages/from/{email}",
+                new JsonSerializerOptions()
+                {
+                    PropertyNameCaseInsensitive = true,
+                    ReferenceHandler = ReferenceHandler.Preserve
+                });
+
             return Page();
         }
     }
