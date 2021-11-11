63fb1128a7f902154f61ff105470b46d147e05a0
disoversity <sinkevicedwin@gmail.com>
2021-11-11 22:52:06 +0200

    Add PostedEventArgs.cs

---
+++ Karma/Services/PostedEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Karma.Models;
+
+namespace Karma.Services
+{
+    public class PostedEventArgs : EventArgs
+    {
+        public Post Post { get; set; }
+        public string UserId { get; set; }
+        public string UserEmail { get; set; }
+    }
+}
