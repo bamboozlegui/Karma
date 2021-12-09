0916e3f01331901aea411be9384355b30a8aa143
disoversity <sinkevicedwin@gmail.com>
2021-12-09 11:03:40 +0200

    Add Fullfilment and Giveaway models

---
+++ Karma/Models/Fullfilment.cs
@@ -0,0 +1,29 @@
+﻿using Karma.Areas.Identity.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Karma.Models
+{
+    [Index(nameof(Fulfillment.Id), IsUnique = true)]
+    public class Fulfillment
+    {
+        public enum FulfillmentEnum
+        {
+            InProgress = 0,
+            Done = 1,
+        }
+
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public KarmaUser Fulfiller { get; set; }
+        [Required]
+        public RequestPost Request { get; set; }
+        [Required]
+        public  FulfillmentEnum State { get; set; }
+    }
+}
---
+++ Karma/Models/Giveaway.cs
@@ -0,0 +1,25 @@
+﻿using Karma.Areas.Identity.Data;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Karma.Models
+{
+    [Index(nameof(Giveaway.Id), IsUnique = true)]
+    public class Giveaway
+    {
+        public enum GiveawayEnum
+        {
+            InProgress = 0,
+            Done = 1,
+        }
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public KarmaUser Receiver { get; set; }
+        [Required]
+        public ItemPost Item { get; set; }
+        [Required]
+        public GiveawayEnum State { get; set; }
+    }
+}
