f682dcc4f2715fdc1b1692ee7479e6cc1850937a
karoluzas <liskaro23@gmail.com>
2021-12-10 09:43:29 +0200

    testin

---
+++ KarmaService.Tests/RepositoryTests.cs
@@ -0,0 +1,26 @@
+﻿using Karma.Data;
+using Karma.Models;
+using Karma.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KarmaService.Tests
+{
+    [TestClass]
+    class RepositoryTests
+    {       
+        [TestMethod]
+        public async Task AddPostAsync_Test()
+        {
+            var sqlItemRepository = new Mock<SqlItemRepository>();
+            var itemPost = new ItemPost();
+
+            sqlItemRepository.Setup( x =>  x.AddPostAsync(itemPost, "labas")).ReturnsAsync(itemPost);
+        }
+    }
+}
